1665219a1a00e7713861cf8303310ee7689e9200
Yona-TYT <yonatan.el.amigo@gmail.com>
2024-11-24 22:58:32 -0400

    Revert "Listen and Send message chat"
    This reverts commit c8eddd9e9f64d07974c718afb7144848952b4ad0.

--- src/simutrans/simmesg.cc
+++ src/simutrans/simmesg.cc
@@ -17,7 +17,6 @@
 #include "dataobj/translator.h"
 #include "dataobj/environment.h"
 #include "dataobj/pakset_manager.h"
-#include "dataobj/scenario.h"
 #include "network/network_socket_list.h"
 #include "player/simplay.h"
 #include "utils/simstring.h"
@@ -414,7 +413,6 @@ void chat_message_t::add_chat_message(const char* text, sint8 channel, sint8 sen
 					sound_play(sound_desc_t::message_sound, 255, TOOL_SOUND);
 				}
 			}
-			welt->get_scenario()->is_message_chat(channel, player, sender_nick.c_str(), recipient.c_str(), pos, text);
 		}
 	}
 	else if (channel == -2) {
